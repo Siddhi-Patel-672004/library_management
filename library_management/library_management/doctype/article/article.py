37d12816cf27a788e2696d1bd4785229ddfad1fd
Siddhi Patel <siddhi.patel672004@gmail.com>
2025-05-12 10:38:30 +0000

    Updated Article Doctype

--- library_management/library_management/doctype/article/article.py
+++ library_management/library_management/doctype/article/article.py
@@ -2,10 +2,10 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Article(Document):
+class Article(WebsiteGenerator):
 	pass
 
 def get_permission_query_conditions(user):
